6094f2c29f7d648b5872f262cd984414c3987eda
tvio <tvio@centrum.cz>
2020-03-29 23:11:16 +0200

    nastaveny linter+ts+prettier a test fastify

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import * as fastify from 'fastify'
+import * as fastifyBlipp from 'fastify-blipp'
+import { Server, IncomingMessage, ServerResponse } from 'http'
+import statusRoutes from './routes'
+const server: fastify.FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify()
+
+server.register(fastifyBlipp)
+server.register(statusRoutes)
+
+const start = async () => {
+    try {
+        await server.listen(3000)
+        server.blipp()
+    } catch (err) {
+        console.log(err)
+        server.log.error(err)
+        process.exit(1)
+    }
+}
+
+process.on('uncaughtException', (error) => {
+    console.error(error)
+})
+process.on('unhandledRejection', (error) => {
+    console.error(error)
+})
+start()
