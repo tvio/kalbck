b00c7ba1f6d2a3c98d497904fcce7028f993f25a
tvio <tvio@centrum.cz>
2020-05-21 21:21:40 +0200

    heroku port

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ const start = async (): Promise<void> => {
         await db.connect(settings.postgresql)
         await db.mon()
 
-        await server.listen(3001)
+        await server.listen(settings.port || 3001)
         server.blipp()
     } catch (err) {
         console.log(err)
