cd5ccd0257d28ace283362e774c965ff295d9f4d
tvio <tvio@centrum.cz>
2020-05-21 21:57:18 +0200

    pg

--- sql/pg.sql
+++ sql/pg.sql
@@ -1,10 +1,24 @@
--- Table: kal.chaty
 
--- DROP TABLE kal.chaty;
+CREATE SEQUENCE kal.chatyidseq
+    INCREMENT 1
+    START 4
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+
+CREATE SEQUENCE kal.denidseq
+    INCREMENT 1
+    START 12
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+
 
 CREATE TABLE kal.chaty
 (
-    id integer NOT NULL DEFAULT nextval('chatyidseq'::regclass),
+    id integer NOT NULL DEFAULT nextval('kal.chatyidseq'::regclass),
     datum date NOT NULL,
     denid integer,
     kdo text COLLATE pg_catalog."default" NOT NULL,
@@ -14,9 +28,6 @@ CREATE TABLE kal.chaty
 
 TABLESPACE pg_default;
 
-ALTER TABLE kal.chaty
-    OWNER to kal;
-
 
 -- Table: kal.dny
 
@@ -24,7 +35,7 @@ ALTER TABLE kal.chaty
 
 CREATE TABLE kal.dny
 (
-    id integer NOT NULL DEFAULT nextval('denidseq'::regclass),
+    id integer NOT NULL DEFAULT nextval('kal.denidseq'::regclass),
     datum date NOT NULL,
     den text COLLATE pg_catalog."default" NOT NULL,
     kdo text COLLATE pg_catalog."default",
@@ -39,7 +50,7 @@ CREATE TABLE kal.dny
 TABLESPACE pg_default;
 
 ALTER TABLE kal.dny
-    OWNER to kal;
+    OWNER to kblwfosqcpwqon;
 
 -- Table: kal.logy
 
@@ -58,36 +69,3 @@ CREATE TABLE kal.logy
 )
 
 TABLESPACE pg_default;
-
-ALTER TABLE kal.logy
-    OWNER to kal;
-
-
--- SEQUENCE: kal.chatyidseq
-
--- DROP SEQUENCE kal.chatyidseq;
-
-CREATE SEQUENCE kal.chatyidseq
-    INCREMENT 1
-    START 4
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    CACHE 1;
-
-ALTER SEQUENCE kal.chatyidseq
-    OWNER TO kal;
-
-
--- SEQUENCE: kal.denidseq
-
--- DROP SEQUENCE kal.denidseq;
-
-CREATE SEQUENCE kal.denidseq
-    INCREMENT 1
-    START 12
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    CACHE 1;
-
-ALTER SEQUENCE kal.denidseq
-    OWNER TO kal;
